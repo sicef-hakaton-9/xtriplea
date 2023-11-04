54833828e9d1ef5288754300ba612f4ac0430fee
AsterixMitic <aca.mitic003@gmail.com>
2023-11-04 19:27:16 +0100

    Initial commit third time

---
+++ app/src/main/java/com/example/roof/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.roof
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roof.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.testText.setText("Nije main")
+    }
+}
