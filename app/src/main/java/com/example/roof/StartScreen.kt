9eb1ca777e9a46ec402fe85b756a9d6de73b68cc
StewKI <andrijastev4@gmail.com>
2023-11-04 22:40:36 +0100

    start screen fix by coa

--- app/src/main/java/com/example/roof/StartScreen.kt
+++ app/src/main/java/com/example/roof/StartScreen.kt
@@ -1,11 +1,22 @@
 package com.example.roof
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roof.databinding.ActivityStartScreenBinding
 
 class StartScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_screen)
+        binding = ActivityStartScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.goButton.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
+
     }
 }
