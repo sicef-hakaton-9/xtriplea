f9ac11628caed0265aef60a666a0742d656dd009
AsterixMitic <aca.mitic003@gmail.com>
2023-11-05 09:30:03 +0100

    ChatGPT API Implemented

--- app/src/main/java/com/example/roof/CurrentScore.kt
+++ app/src/main/java/com/example/roof/CurrentScore.kt
@@ -1,31 +1,29 @@
 package com.example.roof
+
+
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.CheckBox
-import android.widget.TextView
-
-import android.widget.Toast
-import com.example.roof.models.Building
-
-
 import androidx.appcompat.app.AppCompatActivity
 import com.example.roof.databinding.ActivityCurrentScoreBinding
-import com.google.android.gms.maps.model.LatLng
+import com.example.roof.models.Building
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Headers
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.json.JSONObject
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 
 class CurrentScore : AppCompatActivity() {
-
     private lateinit var binding: ActivityCurrentScoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,58 +32,12 @@ class CurrentScore : AppCompatActivity() {
         setContentView(view)
 
         binding.buttonGPT.setOnClickListener{
+            getGPTResponse {
 
-            //TODO: Uzeti pravu lokaciju a ne neku izmisljenu
-            var loc = LatLng(30.0,30.0)
-            val url = "https://api.waqi.info/feed/here/?token=d7f28bd96ed37a0c3a27143627a44499b8a1405c";
-
-            val request = Request.Builder().url(url).build()
-            val client = OkHttpClient();
-
-           client.newCall(request).enqueue(object: Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    e.printStackTrace()
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    Log.d("Response","Recieved response")
-
-
-                    if(!response.isSuccessful){
-                        Log.e("Error","Neka greska")
-                        val body = response?.body?.toString()
-                        Log.e("ErrorResponse", body!!)
-
-                    }
-                    else {
-
-                        val body = response?.body?.string()
-                        //val aqi = json.get("data");
-                        Log.d("ResponseBody", body!!)
-
-
-                    }
-
-
-
-                }
-
-            })
-
-
-
+            }
         }
 
         val building = intent.getSerializableExtra("bld") as Building
-
-        //for (b in instance.app.buildings){
-        //    Log.d("AndrijaKaze", b.address)
-        //}
-
-        //Log.d("String se pasuje", strng!!)
-        //if(strng != null){
-            Toast.makeText(this, building.yearBuiltIn.toString(), Toast.LENGTH_LONG).show()
-        //}
 
         binding.tvYear.text = building.yearBuiltIn.toString()
         binding.tvSqm.text = building.surfaceArea.toString()
@@ -100,6 +52,63 @@ class CurrentScore : AppCompatActivity() {
         binding.tvTrafficFlow.text = building.trafficFlow().toString()
 
 
+    }
+
+    private fun getGPTResponse(callback: (String)->Unit) {
+        val secretStr = "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"
+        val url = "https://api.openai.com/v1/chat/completions";
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .build();
+
+        val jsonPayload = """{
+  "model": "gpt-3.5-turbo",
+  "max_tokens": 1000,
+  "messages": [{"role": "user", "content": "You are virtual assistant that helps users improve their building by giving them advices on how to be more energy efficient and how could you also save them money. Give them 3 options to chose form, and write each of them in form of short text. Options should not have more than 20 words and also have pricing for each of them"}]}"""
+
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val body: RequestBody = JSONObject(jsonPayload).toString().toRequestBody()
+
+        val headers = Headers.Builder()
+            .add("Authorization", "Bearer $secretStr")
+            .add("Content-Type", "application/json")
+            .build()
+
+        val request = Request.Builder().url(url).headers(headers).post(body).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("Response", "Recieved response")
+
+                if (!response.isSuccessful) {
+                    Log.e("Error", "Neka greska")
+                    val body = response?.body?.string()
+                    Log.e("ErrorResponse", body!!)
+
+                } else {
+
+                    val body = response?.body?.string()
+                    //val aqi = json.get("data");
+                    Log.d("ResponseBody", body!!)
+                    //val jsonObject = JSONObject(body)
+                    //val jsonArray = jsonObject.getJSONObject("data")
+                    //val text = jsonArray.getString("aqi").toString()
+                    //binding.airQualityTextView.setText(text)
+                    callback("test")
+
+                }
+
+
+            }
+
+        })
     }
 
     fun goMap(view: View) {
